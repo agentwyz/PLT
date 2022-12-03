a7954771080e4aebd7e7594edff8dae5f8a9f40c
phpwyz <2012663060@qq.com>
2022-12-03 15:22:31 +0800

    add some content

--- match.ml
+++ match.ml
@@ -7,7 +7,30 @@ let b =
     match 42 with 
     | foo -> foo;;
 
-let z =
+let c =
     match "foo" with
     | "bar" -> 0
     | _ -> 1;;
+
+let d =
+    match [] with
+    | [] -> "empty"
+    | _ -> "not empty";;
+
+let e =
+    match [1:2] with
+    | [] -> "empty"
+    | _ -> "not empty";;
+
+
+(*注意模式匹配并行是需要等价的*)
+let f = 
+    match ["toylor"; "swift"] with
+    | [] -> "folkore"   (*这个地方是字符串*)
+    | h :: t -> h;;     (*这个地方是也需要是字符串*)
+
+let fst3 t =
+    match t with
+    | (a, b, c) -> a;;
+
+
